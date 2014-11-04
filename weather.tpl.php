5f6167f4c20df1f30c57eef8ebc26eee46142fd2
Luca Lusso <llusso@wellnet.it>
2014-11-04 09:46:59 +0100

    Add cache

--- weather.tpl.php
+++ weather.tpl.php
@@ -1,3 +1,4 @@
 <img src="http://openweathermap.org/img/w/<?php print $data->weather[0]->icon;?>.png" />
 <h3><?php print $data->name;?></h3>
 <p><?php print $data->weather[0]->main;?><br/><?php print $data->weather[0]->description;?></p>
+<p>Last updated: <?php print format_date($timestamp, 'custom', 'm/d/Y - H:i:s'); ?></p>
